0f03f9ddae4884625e74d384561810580155fe31
Aegean <egeaybars1102@gmail.com>
2024-06-12 11:00:37 +0300

    simple storage

---
+++ index.js
@@ -0,0 +1,33 @@
+import { Account, RpcProvider, json, Contract } from 'starknet';
+import fs from 'fs';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const privateKey = process.env.PRIVATE_KEY;
+
+const provider = new RpcProvider({ nodeUrl: 'https://starknet-sepolia.public.blastapi.io' });
+
+const accountAddress = '0x067981c7F9f55BCbdD4e0d0a9C5BBCeA77dAcB42cccbf13554A847d6353F728e';
+
+const account = new Account(provider, accountAddress, privateKey, "1");
+
+const compiledContractAbi = json.parse(
+    fs.readFileSync('./abi.json').toString('ascii')
+);
+
+const contractAddress = '0x01bb7d67375782ab08178b444dbda2b0c1c9ff4469c421124f54e1d8257f2e97';
+const storageContract = new Contract(compiledContractAbi.abi, contractAddress, provider);
+
+storageContract.connect(account);
+
+let getData = await storageContract.get();
+console.log('Stored_data before set():', getData.toString());
+
+
+const myCall = storageContract.populate('set', [59]);
+const res = await storageContract.set(myCall.calldata);
+await provider.waitForTransaction(res.transaction_hash);
+
+
+getData = await storageContract.get();
+console.log('Stored_data after set():', getData.toString());
