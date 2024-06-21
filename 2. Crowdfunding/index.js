ffee47642c9f8032a88d93b728a9e89e8d1e3f83
Aegean <egeaybars1102@gmail.com>
2024-06-21 12:16:34 +0300

    update crowdfunding script + more tutorial content

--- 2. Crowdfunding/index.js
+++ 2. Crowdfunding/index.js
@@ -6,8 +6,8 @@ dotenv.config();
 const provider = new RpcProvider({ nodeUrl: 'https://free-rpc.nethermind.io/sepolia-juno' });
 //0x05b0D30B02349c93b30e02f1A0C3F3F44914D9fbA691581a7Cdfe11f88972853
 //0x067981c7F9f55BCbdD4e0d0a9C5BBCeA77dAcB42cccbf13554A847d6353F728e
-const accountAddress = '0x067981c7F9f55BCbdD4e0d0a9C5BBCeA77dAcB42cccbf13554A847d6353F728e';
-const privateKey = process.env.PRIVATE_KEY;
+const accountAddress = '0x05b0D30B02349c93b30e02f1A0C3F3F44914D9fbA691581a7Cdfe11f88972853';
+const privateKey = process.env.PRIVATE_KEY_OTHER;
 
 const account = new Account(provider, accountAddress, privateKey, "1");
 
@@ -22,7 +22,7 @@ const compiledCrowdFundAbi = json.parse(
 //Initialize contract objects - Crowdfunding and ETH contract (for approving ETH to transfer to our contract)
 const ETHAddress = '0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7'; //Starknet Sepolia ETH Contract Address
 const tokenContract = new Contract(compiledERC20Abi, ETHAddress, provider);
-const crowdfundingAddr = '0x06df0d52fe6157eac91f2788c2247be1870fd4e73355e65b228b5fa886f762a8';
+const crowdfundingAddr = '0x075ab72714fac0685449dd9a75e76db59dc0ff9b146afaff5450e2af7155ef74';
 const crowdfundingContract = new Contract(compiledCrowdFundAbi.abi, crowdfundingAddr, provider);
 
 tokenContract.connect(account);
@@ -46,27 +46,15 @@ async function contribute(campaign_no, amount) {
     const contributeCall = crowdfundingContract.populate('contribute',
         [campaign_no, amountInWei]);
 
-    const multiCall = await account.execute([
-        {
-            contractAddress: ETHAddress,
-            entrypoint: 'approve',
-            calldata: approveCall.calldata,
-        },
-        {
-            contractAddress: crowdfundingAddr,
-            entrypoint: 'contribute',
-            calldata: contributeCall.calldata,
-        },
-    ], compiledCrowdFundAbi);
+    const multiCall = await account.execute([approveCall, contributeCall]);
 
     await provider.waitForTransaction(multiCall.transaction_hash);
     console.log('Contributed to the campaign_no ', campaign_no);
 }
 
 async function getFunderContribution(campaign_no, funder_addr) {
-    const identifier_hash = await crowdfundingContract.get_funder_identifier(campaign_no, funder_addr);
-    const contribution = await crowdfundingContract.get_funder_contribution(identifier_hash);
-    console.log('Funder Contribution: ', contribution.toString());
+    const funder_info = await crowdfundingContract.get_funder_info(campaign_no, funder_addr);
+    console.log('Funder Contribution: ', funder_info.amount_funded.toString());
 }
 
 async function getLatestCampaignNo() {
@@ -101,14 +89,14 @@ async function withdrawFunds(campaign_no) {
 }
 
 //getLatestCampaignNo();
-//createCampaign('TestCampaign', accountAddress, ETHAddress, 0.001);
-//contribute(2, 0.1);
-getFunderContribution(2, accountAddress);
+//createCampaign('StarkTest', accountAddress, ETHAddress, 0.001);
+//contribute(1, 0.001);
+//getFunderContribution(1, accountAddress);
 //withdrawContribution(2);
 //withdrawFunds(1)
 
-const balance = await tokenContract.balanceOf(accountAddress)
-console.log(balance)
+//const balance = await tokenContract.balanceOf(crowdfundingAddr)
+//console.log(balance)
 
 //getFunderInfo(1, accountAddress)
-//getCampaignInfo(1)
+getCampaignInfo(1)
